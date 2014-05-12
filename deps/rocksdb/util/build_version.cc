a74129aa4a7992648c04de9af3c06dd4cc24441d
crnsnlzc <lzc6@vip.qq.com>
2014-05-12 16:53:45 +0800

    不知道这个build_version有啥用

--- deps/rocksdb/util/build_version.cc
+++ deps/rocksdb/util/build_version.cc
@@ -1,5 +1,5 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:5ad3651f9214b5acfff65658db8a537c383d5825";
-const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Fri May  2 10:03:30 CST 2014";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:84b4854914dcb19f06dd1daf486c42cc1f89fd3f";
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Mon May 12 16:52:03 CST 2014";
 const char* rocksdb_build_compile_date = __DATE__;
 const char* rocksdb_build_compile_time = __TIME__;
